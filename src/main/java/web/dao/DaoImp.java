0216dd29ccd83fd3121ebd9ca06ee9a0f25b2c32
Alex-gid <aleksandrcerkasov8312@gmail.com>
2023-09-08 03:19:35 +0300

    Initial commit

---
+++ src/main/java/web/dao/DaoImp.java
@@ -0,0 +1,39 @@
+package web.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import web.model.User;
+
+import java.util.List;
+@Repository
+public class DaoImp implements Dao{
+
+    @Autowired
+    private SessionFactory sessionFactory;
+    @Override
+    public List<User> getAllUsers() {
+        return sessionFactory.getCurrentSession().createQuery("from User", User.class).getResultList();
+    }
+
+    @Override
+    public void saveUser(User user) {
+        sessionFactory.getCurrentSession().saveOrUpdate(user);
+    }
+
+    @Override
+    public User getUser(int id) {
+        return sessionFactory.getCurrentSession().get(User.class, id);
+    }
+
+    @Override
+    public void deleteUser(int id) {
+        sessionFactory.getCurrentSession()
+                .createQuery("delete from User where id =:userId")
+                .setParameter("userId", id)
+                .executeUpdate();
+    }
+
+
+}
